5e7f2c240d7953b719543c295a0885cb2c8cf3be
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2023-01-06 19:00:38 +0200

    Work_Mod_5-10___23_(Jest)

--- __tests__/sampleTest.js
+++ __tests__/sampleTest.js
@@ -1,7 +1,14 @@
 // const { italic } = require("colors");
 
 const addTwoNumbers = (a, b) => {
-    return a + b;
+    try {
+        if (!a || !b) {
+            throw new Error();
+        }
+        return a + b;
+    } catch (err) {
+        throw new Error("Please provide valid numbers!")
+    }
 }
 
 //! Test suite (Набор тестов)
@@ -19,5 +26,12 @@ describe('Additional service test', () => {
         const secondNumber = 1;
         const addTwoNumbersReult = addTwoNumbers(firstNumber, secondNumber);
         expect(addTwoNumbersReult).toEqual(firstNumber + secondNumber);
+    });
+    //! Третий Unit-Test case
+    it("3.addTwoNumbers returns error in case of valid params", () => {
+        // const firstNumber = null;
+        const secondNumber = 1;
+        // const addTwoNumbersReult = addTwoNumbers(firstNumber, secondNumber);
+        expect(() => addTwoNumbers(secondNumber)).toThrow("Please, provide valid numbers!!!");
     });
 });
