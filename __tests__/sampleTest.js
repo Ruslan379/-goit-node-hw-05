e9373f453082c31ae6f58f985257f22c9baa2457
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2023-01-06 17:59:44 +0200

    Work_Mod_5-10___19_(Jest)

--- __tests__/sampleTest.js
+++ __tests__/sampleTest.js
@@ -1,3 +1,9 @@
-describe('Additional service test', function () {
+const { italic } = require("colors");
 
+//! Test suite (Набор тестов)
+describe('Additional service test', () => {
+    //! Один Unit-Test
+    it("Add two valid numbers", () => {
+        expect(1 + 1).toEqual(2);
+    })
 });
