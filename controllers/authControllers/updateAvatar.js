441a53b130ea26176c9ebc9d05ed324f3b6d1cad
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2023-01-08 19:19:32 +0200

    Work_Mod_5-9.dopmat-HW-5___41_Jimp

--- controllers/authControllers/updateAvatar.js
+++ controllers/authControllers/updateAvatar.js
@@ -1,6 +1,8 @@
 const { User } = require("../../models");
 const path = require("path");
 const fs = require("fs/promises");
+//! Jimp
+const Jimp = require('jimp');
 
 
 //----------------------------------------------------------------------------------
@@ -19,9 +21,47 @@ const updateAvatar = async (req, res) => {
         await fs.rename(tempUpload, resultUpload);
 
         const avatarURL = path.join("public", "avatars", imageName);
-        // await User.findByIdAndUpdate(req.user._id, { avatarURL });
+        console.log();
+
+        //! Jimp
+        // Jimp.read(avatarURL)
+        //     .then(avatarURL => {
+        //         return image
+        //             .resize(250, 250) // resize
+        //             .quality(60) // set JPEG quality
+        //             .greyscale() // set greyscale
+        //             // .write(`Jimp_${imageName}`); // save
+        //             .write("250x250.png"); // save
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        // });
+
+        //----------------------------------------------------------------------------
+        //! Jimp
+        async function resizeAvatar() {
+            //! Read the image.
+            const image = await Jimp.read(avatarURL);
+            //! 2-вариант
+            await image
+                .resize(250, 250)
+                .quality(60) // set JPEG quality
+                .greyscale() // set greyscale
+                .writeAsync(`./tmp/Jimp_${imageName}_250x250.png`); //! Save and overwrite the image
+            // .writeAsync(`${imageName}_150x150.png`); //! записывает в корень папки проекта ./
+            //! 1-вариант
+            // const image = await Jimp.read('https://images.pexels.com/photos/298842/pexels-photo-298842.jpeg');
+            // await image.resize(250, 250);
+            // await image.greyscale() // set greyscale
+            // await image.writeAsync(`./tmp/${imageName}_250x250.png`); //! Save and overwrite the image
+            // console.log("image:", image); //!
+        }
+        resizeAvatar();
+
+        //----------------------------------------------------------------------------
 
         await User.findByIdAndUpdate(req.user._id, { avatarURL });
+        // await User.findByIdAndUpdate(req.user._id, { avatarURL }, { new: true });
 
         res.json({ avatarURL });
 
