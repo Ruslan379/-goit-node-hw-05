56d4c4a873bb9c737b7b692dccedcca2232a9c43
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2023-01-09 20:52:52 +0200

    Final_Mod_5-9.dopmat_HW-5+Lib-Jimp___67_mongoose.set("strictQuery", false)

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false); //!!!!!
 
 const app = require('./app')
 
@@ -16,3 +17,4 @@ mongoose.connect(DB_HOST)
     console.log(error.message);
     process.exit(1); //? закрыть все неиспользуемые процессы
   });
+
