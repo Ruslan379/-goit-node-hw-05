176d8ba4ab966ca803c3885537144a7cd4c38a8d
Ruslan379 <arnov@i.ua>
2023-01-09 17:06:53 +0200

    Final_Mod_5-9.dopmat_HW-5+Jimp___59_!!!!!!

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,11 +3,13 @@ const controllerWrapper = require("./controllerWrapper")
 const isValidId = require("./isValidId")
 const authMiddleware = require("./authMiddleware")
 const uploadMiddleware = require("./uploadMiddleware")
+const jimpResize250QualBWMiddleware = require("./jimpResize250QualBWMiddleware")
+
 
 module.exports = {
     validation,
     controllerWrapper,
     isValidId,
     authMiddleware,
-    uploadMiddleware,
+    jimpResize250QualBWMiddleware,
 }
