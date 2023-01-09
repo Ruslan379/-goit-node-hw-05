23fa5642be27a948d001ee083bed4c69c459a590
Ruslan379 <arnov@i.ua>
2023-01-09 17:17:41 +0200

    Final_Mod_5-9.dopmat_HW-5+Jimp___62_!!!!!!

--- middlewares/jimpResize250QualBWMiddleware.js
+++ middlewares/jimpResize250QualBWMiddleware.js
@@ -2,7 +2,6 @@ const { resizeAvatarJimp } = require("../helpers")
 
 
 const jimpResize250QualBWMiddleware = async (req, res, next) => {
-    console.log("ПОЛНЫЙ путь к новому Jimp-файлу аватара во временной папке output -> avatarTempURL:".bgWhite.black, avatarTempURL.bgBlue); //!;
     console.log("");
 
     const { path: tempUpload, destination, originalname } = req.file;
@@ -13,7 +12,7 @@ const jimpResize250QualBWMiddleware = async (req, res, next) => {
     console.log("");
     console.log("ПОЛНЫЙ путь к ориг. файлу аватара во временной папке tmp -> tempUpload:".bgBlue, tempUpload.red); //!;
     console.log("");
-    // await resizeAvatarJimp(avatarTempURL);
+
     await resizeAvatarJimp(tempUpload);
     next();
 };
