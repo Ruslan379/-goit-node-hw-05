bb23e19fc2f6d683d7bb73e018e9a6f47de62343
Ruslan379 <arnov@i.ua>
2023-01-09 16:47:18 +0200

    Final_Mod_5-9.dopmat_HW-5+Jimp___56_!!!!!!

--- routes/api/filesRouter.js
+++ routes/api/filesRouter.js
@@ -66,7 +66,15 @@ const uploadMiddleware = multer({ storage });
 const resizeJimpMiddleware = async (req, res, next) => {
     console.log("ПОЛНЫЙ путь к новому Jimp-файлу аватара во временной папке output -> avatarTempURL:".bgWhite.black, avatarTempURL.bgBlue); //!;
     console.log("");
-    await resizeAvatarJimp(avatarTempURL);
+    const { path: tempUpload, destination, originalname } = req.file;
+    console.log("ОБЪЕКТ -> req.file:".red, req.file); //!;
+    console.log("");
+    console.log("ПОЛНЫЙ путь к временной папке tmp -> destination:".bgYellow.black, destination.yellow); //!;
+    console.log("");
+    console.log("ПОЛНЫЙ путь к ориг. файлу аватара во временной папке tmp -> tempUpload:".bgBlue, tempUpload.red); //!;
+    console.log("");
+    // await resizeAvatarJimp(avatarTempURL);
+    await resizeAvatarJimp(tempUpload);
     next();
 };
 
