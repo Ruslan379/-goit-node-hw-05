059b36d29b4e60168d3d7d1b2345cbd901827a6c
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2023-01-10 10:56:51 +0200

    Final_Mod_5-9.dopmat_HW-5+Lib-Jimp___69

--- server.js
+++ server.js
@@ -20,9 +20,28 @@ const { DB_HOST, PORT = 3000 } = process.env;
 //   });
 
 //! 2-вариант NEW
-async function main() {
+// async function main() {
+//   try {
+//     await mongoose.connect(DB_HOST);
+//     app.listen(PORT);
+//     console.log(`Server is running on the port: ${PORT}`.bgGreen.red);
+//     console.log(`Start HW-5 --> DRAFT`.bgRed.green);
+//     console.log("Database connection successful".bgBlue.yellow);
+//     console.log("---------------------------------------".yellow);
+//   } catch (error) {
+//     console.log(error.message);
+//     process.exit(1); //? закрыть все неиспользуемые процессы
+//   }
+// };
+// main();
+
+
+//! 3-вариант NEW
+// async function main() {
+(() => {
   try {
-    await mongoose.connect(DB_HOST);
+    // await mongoose.connect(DB_HOST);
+    mongoose.connect(DB_HOST);
     app.listen(PORT);
     console.log(`Server is running on the port: ${PORT}`.bgGreen.red);
     console.log(`Start HW-5 --> DRAFT`.bgRed.green);
@@ -32,6 +51,5 @@ async function main() {
     console.log(error.message);
     process.exit(1); //? закрыть все неиспользуемые процессы
   }
-};
+})();
 
-main();
