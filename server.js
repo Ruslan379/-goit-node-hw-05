cc085eb8166c06777df6780e74241e5887fef775
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2023-01-13 22:16:29 +0200

    ITeam___85

--- server.js
+++ server.js
@@ -34,6 +34,33 @@ const { DB_HOST, PORT = 3000 } = process.env;
 //   }
 // };
 // main();
+//! +++++++++++++++++++++++++ ITeam ++++++++++++++++++++++++++++
+let x = 12
+let y = 21
+console.log("ДО:    ", "x =", x, "y =", y);
+//--------
+x = x + y;
+y = x - y;
+x = x - y;
+console.log("ПОСЛЕ: ", "x =", x, "y =", y);
+
+const arr2 = { a: 1, b: 2 };
+const arr3 = { a: 1, b: 2, c: 3 };
+const arr4 = { a: 1, b: 2, c: 3, d: 4 };
+
+//! Количество входных данных n
+const lengthАrr = Object.keys(arr4).length;
+const n = 5
+console.log("Количество входных данных --> n =", n);
+// console.log("Количество входных данных --> n =", lengthАrr);
+
+
+const timeComplexity = parseInt(((n + 1) / 2), 10) * 3;
+// const timeComplexity = parseInt(((lengthАrr + 1) / 2), 10) * 3;
+console.log("timeComplexity =", timeComplexity);
+
+//! +++++++++++++++++++++++++ ITeam ++++++++++++++++++++++++++++
+
 
 
 //! 3-вариант NEW
@@ -46,6 +73,7 @@ const { DB_HOST, PORT = 3000 } = process.env;
     console.log(`Start HW-5 --> DRAFT`.bgRed.green);
     console.log("Database connection successful".bgBlue.yellow);
     console.log("---------------------------------------".yellow);
+
   } catch (error) {
     console.log(error.message);
     process.exit(1); //? закрыть все неиспользуемые процессы
