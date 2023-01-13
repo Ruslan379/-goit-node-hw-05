7bce63690251c2ccba6de7669fd3670959479a32
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2023-01-13 22:24:41 +0200

    ITeam___86

--- server.js
+++ server.js
@@ -37,28 +37,35 @@ const { DB_HOST, PORT = 3000 } = process.env;
 //! +++++++++++++++++++++++++ ITeam ++++++++++++++++++++++++++++
 let x = 12
 let y = 21
-console.log("ДО:    ", "x =", x, "y =", y);
+console.log("ДО:    ", "x =", x, "y =", y); // x = 12 y = 21
+
 //--------
 x = x + y;
 y = x - y;
 x = x - y;
-console.log("ПОСЛЕ: ", "x =", x, "y =", y);
+console.log("ПОСЛЕ: ", "x =", x, "y =", y); // x = 21 y = 12
+console.log("");
+//-----------------------------------------------------------------------
 
 const arr2 = { a: 1, b: 2 };
 const arr3 = { a: 1, b: 2, c: 3 };
 const arr4 = { a: 1, b: 2, c: 3, d: 4 };
 
-//! Количество входных данных n
-const lengthАrr = Object.keys(arr4).length;
+//! Количество входных данных: n
+// const n = Object.keys(arr4).length;
 const n = 5
 console.log("Количество входных данных --> n =", n);
 // console.log("Количество входных данных --> n =", lengthАrr);
 
-
+//! Вычислительная временная сложность(time complexity)
 const timeComplexity = parseInt(((n + 1) / 2), 10) * 3;
 // const timeComplexity = parseInt(((lengthАrr + 1) / 2), 10) * 3;
 console.log("timeComplexity =", timeComplexity);
 
+
+//! Вычислительная ёмкостная сложность (space complexity)
+const spaceComplexity = 2 * n;
+console.log("spaceComplexity =", spaceComplexity);
 //! +++++++++++++++++++++++++ ITeam ++++++++++++++++++++++++++++
 
 
